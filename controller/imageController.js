df8b4bafe0e88b073a5cda863bb2f0bbc562a43e
som <sombhud2637@gmail.com>
2023-10-12 09:45:42 +0530

    add delete and caption

--- controller/imageController.js
+++ controller/imageController.js
@@ -5,19 +5,21 @@ import { v2 as cloudinary } from 'cloudinary';
 
 // upload a image 
 export const postImage = async (req, res) => {
-
+    
+            
     try {
         const myCloud = await cloudinary.uploader.upload(req.body.imageData, {
             folder: "sampleFolder",
             width: 150,
             crop: "scale",
         });
-        // console.log(myCloud)
+      console.log(req.body.imageData , req.body.caption)
 
         const image = await Images.create({
 
             public_id: myCloud.public_id,
             url: myCloud.secure_url,
+            caption:req.body.caption
 
         });
         res.status(200).json({ success: true, image })
@@ -41,4 +43,58 @@ export const getAllImages = async (req, res) => {
             message: "Can't get images"
         })
     }
+}
+
+//get unique Image using image Id
+export const getImamgById = async (req,res)=>{
+    const {id}= req.params
+    console.log(id)
+    try{
+        const Image = await Images.findById({_id:id}).exec()
+        res.status(200).json({
+            success:true,
+            Image
+        })
+    }
+    catch(err){
+        res.status(400).json({
+            success:false,
+            message:'Image not find',
+        })
+    }
+
+}
+
+//delete data 
+
+export const deleteImage = async (req , res)=>{
+    const {id } = req.params
+    console.log("delete is",id)
+
+    const {data} = await Images.findById({_id:id}).exec()
+
+    cloudinary.uploader.destroy(data.public_id, (error, result) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log('Image deleted successfully');
+        }
+        
+      });
+
+   try{
+         const data =await Images.findByIdAndDelete({_id:id}).exec()
+         res.status(200).json({
+            success:true,
+            message:'data deleted succesfully'
+         })
+   }
+   catch(err)
+   {
+    res.status(400).json({
+        success:false,
+        message:err
+     })
+   }
+
 }
--- routes/imageRoute.js
+++ routes/imageRoute.js
@@ -1,13 +1,14 @@
 // image route here
 
 import express from 'express';
-import { getAllImages, postImage } from '../controller/imageController.js';
+import { getAllImages, postImage , getImamgById, deleteImage } from '../controller/imageController.js';
 
 
 const router = express.Router();
 
 router.post('/', postImage)
 router.get('/', getAllImages)
-
+router.get('/:id', getImamgById)
+router.delete('/:id ', deleteImage)
 
 export default router
